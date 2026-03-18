b55df5dff16f278e6dfb57804aeffde43b897319
Lerato <lerato.rakgotho@gmail.com>
2026-03-18 23:52:46 +0200

    Add project files.

---
+++ OneElevenWebApi/Controllers/StringSortController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OneElevenWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringSortController : ControllerBase
+    {
+        
+
+        [HttpPost]
+        public IActionResult SortCharacters([FromBody] InputModel input)
+        {
+            if (input == null || string.IsNullOrEmpty(input.Data))
+            {
+                return BadRequest("Data field is required");
+            }
+            var characters = input.Data.ToCharArray();
+            var sortedCharacters = characters.OrderBy(c => c).Select(c => c.ToString()).ToArray();
+            return Ok(new { word = sortedCharacters });
+
+        }
+    }
+
+    public class InputModel
+    {
+        public string Data { get; set; }
+    }
+}
